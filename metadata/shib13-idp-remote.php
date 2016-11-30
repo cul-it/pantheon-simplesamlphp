659075b32314463453c8dd9b62280138b2842dfc
James Reidy <jreidy@cornell.edu>
2016-11-30 10:30:22 -0500

    decide which version of shib13-idp to us based on Pantheon env variables

--- metadata/shib13-idp-remote.php
+++ metadata/shib13-idp-remote.php
@@ -1,15 +1,17 @@
 <?php
-/**
- * SAML 1.1 remote IdP metadata for simpleSAMLphp.
- *
- * Remember to remove the IdPs you don't use from this file.
- *
- * See: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-idp-remote
- */
 
-/*
-$metadata['theproviderid-of-the-idp'] = array(
-	'SingleSignOnService'  => 'https://idp.example.org/shibboleth-idp/SSO',
-	'certFingerprint'      => 'c7279a9f28f11380509e072441e3dc55fb9ab864',
-);
-*/
+/**
+ * select which version to include based on Pantheon environmental variables
+ * original contents of this file now in shib13-idp-remote.php.default
+ */
+if (defined('PANTHEON_ENVIRONMENT') && !empty($_ENV['PANTHEON_ENVIRONMENT'])) {
+    if ($_ENV['PANTHEON_ENVIRONMENT'] == 'live') {
+        require 'shib13-idp-remote.php.prod';
+    }
+    else {
+        require 'shib13-idp-remote.php.test';
+    }
+}
+else {
+    require 'shib13-idp-remote.php.default';
+}
