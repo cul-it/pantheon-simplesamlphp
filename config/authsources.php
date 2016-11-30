28bb2d2d45e61a79714c97969f0d5e206caa591a
James Reidy <jreidy@cornell.edu>
2016-11-30 10:28:05 -0500

    autthsources comment

--- config/authsources.php
+++ config/authsources.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * select which version to include based on Pantheon environmental variables
+ * original contents of this file now in authsourches.php.default
+ */
+
 if (defined('PANTHEON_ENVIRONMENT') && !empty($_ENV['PANTHEON_ENVIRONMENT'])) {
     if ($_ENV['PANTHEON_ENVIRONMENT'] == 'live') {
         require 'authsources.php.prod';
